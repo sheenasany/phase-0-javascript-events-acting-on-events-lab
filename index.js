114c3ae5d8175340b77c818605151128668e59cd
Sheena Sang <sheenasany@gmail.com>
2023-08-28 12:38:49 -0400

    completed lab

--- index.js
+++ index.js
@@ -1,1 +1,32 @@
-// Your code here
+
+const dodger = document.getElementById("dodger");
+dodger.style.backgroundColor = "#FF69B4";
+dodger.style.bottom = "100px"
+dodger.style.left = "180px"
+
+  function moveDodgerLeft() {
+    const leftNumbers = dodger.style.left.replace("px", "");
+    const left = parseInt(leftNumbers, 10);
+  
+    if (left > 0) {
+      dodger.style.left = `${left - 1}px`;
+    }
+  };
+
+const moveDodgerRight = function() {
+  const leftNumbers = dodger.style.left.replace("px", "");
+  const left = parseInt(leftNumbers, 10);
+
+  if (left <= 360) {
+    dodger.style.left = `${left + 1}px`;
+  }
+}
+
+  document.addEventListener("keydown", function (e) {
+    if (e.key === "ArrowLeft") {
+      moveDodgerLeft();
+    }
+    if (e.key === "ArrowRight") {
+      moveDodgerRight();
+    }
+  });
